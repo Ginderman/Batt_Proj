577118cef80fafeb759acb916d9a00997e267bc2
Eric Ginder <ginderman10@gmail.com>
2024-06-09 05:06:03 -0700

    Added a form and used axios to parse entered information and send the information to a post route in the backend

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,28 @@ import axios from 'axios';
 function App() {
   const [state, setState] = useState("")
 
-  const onClick = () => {
-    axios.get('http://localhost:3050/')
+  const onChange = (e) => {
+   setState(e.target.value)
+   console.log(state);
+    
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    axios.post(`http://localhost:3050/${e.target[0].value}`, {
+        firstName: e.target[1].value,
+        lastName: e.target[2].value,
+        orders: [1, 2, 3],
+       
+      }, {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        params:{
+          value:  e.target[0].value
+        }
+      }
+    )
     .then( response => {
       console.log(response);
     })
@@ -20,7 +40,16 @@ function App() {
 
   return (
     <>
-      <button onClick={onClick}>Click Me!</button>
+      <form onSubmit={onSubmit}>
+      <input type="text" className="inputText1" ></input>
+        <input type="text" className="inputText2" ></input>
+        <input type="text" className="inputText3" ></input>
+
+        
+        <button type="submit">Submit</button>
+      </form>
+
+      {/* <button onClick={onClick}>Click Me!</button> */}
     </>
   )
 }
